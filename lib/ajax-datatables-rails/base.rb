ebb078abaded96a825dd22df56e74b84834a92c8
Dmitry Sychev <dmitry.sychev@me.com>
2021-04-26 13:57:51 -0400

    changed records_total_count to .size

--- lib/ajax-datatables-rails/base.rb
+++ lib/ajax-datatables-rails/base.rb
@@ -117,11 +117,11 @@ module AjaxDatatablesRails
     end
 
     def records_total_count
-      fetch_records.count(:all)
+      fetch_records.length
     end
 
     def records_filtered_count
-      filter_records(fetch_records).size
+      filter_records(fetch_records).length
     end
 
     def global_search_delimiter
